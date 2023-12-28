9ea443b60029b659eeb56a1d414554d74a44f374
“nishanthan-k” <krn.nishanthan@gmail.com>
2023-12-28 13:09:05 +0530

    updated header to display username as User when json is empty

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -7,25 +7,25 @@ const SideBar = () => {
   return (
     <Container>
       <div className='sidebar'>
-        <Menu.Item className='menu-item' as={ Link } name="dashboard" to="/dashboard">
+        <div className='menu-item' as={ Link } name="dashboard" to="/dashboard">
           <Icon size='big' name='chart bar outline' />
           Dashboard
-        </Menu.Item>
+        </div>
 
-        <Menu.Item className='menu-item' as={ Link } name="projects" to="/projects">
+        <div className='menu-item' as={ Link } name="projects" to="/projects">
           <Icon size='big' name='laptop' />
           Projects
-        </Menu.Item>
+        </div>
 
-        <Menu.Item className='menu-item' as={ Link } name="leave" to="/leave">
+        <div className='menu-item' as={ Link } name="leave" to="/leave">
           <Icon size='big' name='calendar check' />
           Leave
-        </Menu.Item>
+        </div>
 
-        <Menu.Item className='menu-item' as={ Link } name="account" to="/account">
+        <div className='menu-item' as={ Link } name="account" to="/account">
           <Icon size='big' name='user circle' />
           Account
-        </Menu.Item>
+        </div>
       </div>
     </Container>
   )
